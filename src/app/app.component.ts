66aca3890e8cae1259240b109b588f827bfbfb5f
krisklas <kristian.klassen@arvato-scs.com>
2022-10-11 16:28:54 +0200

    css adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,75 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Die Komponente würde man CalculatorComponent nennen (und alle Dateien dazu entsprechend)
-  // Außerdem solltest du unbedingt ne Validierung einführen, dass man keinen Quatsch (z.B. 1 + + 1) eingeben kann.
 
-
-  // Properties immer möglichst selbstsprechend. Was ist z.B. a oder b? Musst auch nicht viel abkürzen dabei. Und immer kleingeschrieben und camelCase.
   title = 'Calculator';
   myName = 'Kristian';
   display = '';
   saveNum = '';
-  cache2 = '';
+  cache = '';
   displaySmall = '';
-  cacheArr: string[] = [];
-  resultDis = '';
+  storeArray: string[] = [];
+  finalResult = '';
 
-  // Der Name passt nicht gut. Besser du machst einzelne Methoden für die Operationen, z.B. add, multiply, ... Unter der Haube kannst du dann "chache" (oder besser "store") nutzen.
   // Außerdem ist das schwer zu testen mit Unit-Tests, weil die Methode so groß ist und so viele Möglichkeiten hat, durchlaufen zu werden (zyklomatische Komplexität).
 
   input(input: any): void {
 
-    let Add: string[] = ['+'];
-    let Sub: string[] = ['-'];
-    let Mul: string[] = ['*'];
-    let Div: string[] = ['/'];
+    let addSymbol: string[] = ['+'];
+    let subSymbol: string[] = ['-'];
+    let mulSymbol: string[] = ['*'];
+    let divSymbol: string[] = ['/'];
 
     if (input !== '-' && input !== '+' && input !== '/' && input !== '*') {
       this.saveNum = this.saveNum + input;
       this.display = this.saveNum;
     }
-    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.saveNum === '' && this.resultDis === '') {
-      this.cacheArr = this.cacheArr.concat('0');
-      this.cacheArr = this.cacheArr.concat(input);
-      this.cache2 = input;
-      this.displaySmall = this.cache2;
+    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.saveNum === '' && this.finalResult === '') {
+      this.storeArray = this.storeArray.concat('0');
+      this.storeArray = this.storeArray.concat(input);
+      this.cache = input;
+      this.displaySmall = this.cache;
     }
-    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.saveNum !== '' && this.resultDis === '') {
-      this.cacheArr = this.cacheArr.concat(this.saveNum);
-      this.cacheArr = this.cacheArr.concat(input);
+    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.saveNum !== '' && this.finalResult === '') {
+      this.storeArray = this.storeArray.concat(this.saveNum);
+      this.storeArray = this.storeArray.concat(input);
       this.saveNum = '';
-      this.cache2 = this.cacheArr.join('');
-      this.displaySmall = this.cache2
+      this.cache = this.storeArray.join('');
+      this.displaySmall = this.cache
     }
-    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.resultDis !== '' && this.saveNum === '') {
-      this.cacheArr = this.cacheArr.concat(input);
-      this.cache2 = this.resultDis + input;
-      this.displaySmall = this.cache2;
+    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.finalResult !== '' && this.saveNum === '') {
+      this.storeArray = this.storeArray.concat(input);
+      this.cache = this.finalResult + input;
+      this.displaySmall = this.cache;
     }
-    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.resultDis !== '' && this.saveNum !== '' && (this.cacheArr.slice(-1) === Sub || this.cacheArr.slice(-1) === Add || this.cacheArr.slice(-1) === Div || this.cacheArr.slice(-1) === Mul)) {
-      this.cacheArr = this.cacheArr.concat(this.saveNum);
-      this.cacheArr = this.cacheArr.concat(input);
-      this.cache2 = this.cacheArr.join('');
-      this.displaySmall = this.cache2;
+    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.finalResult !== '' && this.saveNum !== '' && (this.storeArray.slice(-1) === subSymbol || this.storeArray.slice(-1) === addSymbol || this.storeArray.slice(-1) === divSymbol || this.storeArray.slice(-1) === mulSymbol)) {
+      this.storeArray = this.storeArray.concat(this.saveNum);
+      this.storeArray = this.storeArray.concat(input);
+      this.cache = this.storeArray.join('');
+      this.displaySmall = this.cache;
       this.saveNum = '';
     }
-    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.resultDis !== '' && this.saveNum !== '' && (this.cacheArr.slice(-1) !== Sub || this.cacheArr.slice(-1) !== Add || this.cacheArr.slice(-1) !== Div || this.cacheArr.slice(-1) !== Mul)) {
-      this.cacheArr = [];
-      this.resultDis = '';
-      this.cacheArr = this.cacheArr.concat(this.saveNum);
-      this.cacheArr = this.cacheArr.concat(input);
-      this.cache2 = this.cacheArr.join('');
-      this.displaySmall = this.cache2;
+    else if ((input === '-' || input === '+' || input === '/' || input === '*') && this.finalResult !== '' && this.saveNum !== '' && (this.storeArray.slice(-1) !== subSymbol || this.storeArray.slice(-1) !== addSymbol || this.storeArray.slice(-1) !== divSymbol || this.storeArray.slice(-1) !== mulSymbol)) {
+      this.storeArray = [];
+      this.finalResult = '';
+      this.storeArray = this.storeArray.concat(this.saveNum);
+      this.storeArray = this.storeArray.concat(input);
+      this.cache = this.storeArray.join('');
+      this.displaySmall = this.cache;
       this.saveNum = '';
     }
   }
 
   result(): void {
-    this.cacheArr = this.cacheArr.concat(this.saveNum);
-    this.cache2 = this.cache2 + this.saveNum;
+    this.storeArray = this.storeArray.concat(this.saveNum);
+    this.cache = this.cache + this.saveNum;
     this.saveNum = '';
-    this.displaySmall = this.cache2;
+    this.displaySmall = this.cache;
     this.CalcMulDiv();
     this.CalcAddSub();
   }
@@ -82,24 +77,24 @@ export class AppComponent {
     let preOperand = '';
     let afterOperand = '';
     let miniResult = 0;
-    if (this.cacheArr.length >= 3) {
-      for (var i = 0; i < this.cacheArr.length; i++) {
-        if (this.cacheArr[i] === '/') {
-          preOperand = parseFloat(this.cacheArr[i - 1]).toFixed(7);
-          afterOperand = parseFloat(this.cacheArr[i + 1]).toFixed(7);
+    if (this.storeArray.length >= 3) {
+      for (var i = 0; i < this.storeArray.length; i++) {
+        if (this.storeArray[i] === '/') {
+          preOperand = parseFloat(this.storeArray[i - 1]).toFixed(7);
+          afterOperand = parseFloat(this.storeArray[i + 1]).toFixed(7);
           miniResult = this.div(preOperand, afterOperand);
-          this.cacheArr.splice(i + 1, 1);
-          this.cacheArr.splice(i, 1);
-          this.cacheArr.splice(i - 1, 1, miniResult.toString());
+          this.storeArray.splice(i + 1, 1);
+          this.storeArray.splice(i, 1);
+          this.storeArray.splice(i - 1, 1, miniResult.toString());
           this.CalcMulDiv();
         }
-        else if (this.cacheArr[i] === '*') {
-          preOperand = parseFloat(this.cacheArr[i - 1]).toFixed(7);
-          afterOperand = parseFloat(this.cacheArr[i + 1]).toFixed(7);
+        else if (this.storeArray[i] === '*') {
+          preOperand = parseFloat(this.storeArray[i - 1]).toFixed(7);
+          afterOperand = parseFloat(this.storeArray[i + 1]).toFixed(7);
           miniResult = this.mul(preOperand, afterOperand);
-          this.cacheArr.splice(i + 1, 1);
-          this.cacheArr.splice(i, 1);
-          this.cacheArr.splice(i - 1, 1, miniResult.toString());
+          this.storeArray.splice(i + 1, 1);
+          this.storeArray.splice(i, 1);
+          this.storeArray.splice(i - 1, 1, miniResult.toString());
           this.CalcMulDiv();
         }
       }
@@ -111,30 +106,30 @@ export class AppComponent {
     let afterOperand = '';
     let miniResult = 0;
 
-    if (this.cacheArr.length >= 3) {
-      for (var i = 0; i < this.cacheArr.length; i++) {
-        if (this.cacheArr[i] === '+') {
-          preOperand = parseFloat(this.cacheArr[i - 1]).toFixed(7);
-          afterOperand = parseFloat(this.cacheArr[i + 1]).toFixed(7);
+    if (this.storeArray.length >= 3) {
+      for (var i = 0; i < this.storeArray.length; i++) {
+        if (this.storeArray[i] === '+') {
+          preOperand = parseFloat(this.storeArray[i - 1]).toFixed(7);
+          afterOperand = parseFloat(this.storeArray[i + 1]).toFixed(7);
           miniResult = this.add(preOperand, afterOperand);
-          this.cacheArr.splice(i + 1, 1);
-          this.cacheArr.splice(i, 1);
-          this.cacheArr.splice(i - 1, 1, miniResult.toString());
+          this.storeArray.splice(i + 1, 1);
+          this.storeArray.splice(i, 1);
+          this.storeArray.splice(i - 1, 1, miniResult.toString());
           this.CalcAddSub();
         }
-        else if (this.cacheArr[i] === '-') {
-          preOperand = parseFloat(this.cacheArr[i - 1]).toFixed(7);
-          afterOperand = parseFloat(this.cacheArr[i + 1]).toFixed(7);
+        else if (this.storeArray[i] === '-') {
+          preOperand = parseFloat(this.storeArray[i - 1]).toFixed(7);
+          afterOperand = parseFloat(this.storeArray[i + 1]).toFixed(7);
           miniResult = this.sub(preOperand, afterOperand);
-          this.cacheArr.splice(i + 1, 1);
-          this.cacheArr.splice(i, 1);
-          this.cacheArr.splice(i - 1, 1, miniResult.toString());
+          this.storeArray.splice(i + 1, 1);
+          this.storeArray.splice(i, 1);
+          this.storeArray.splice(i - 1, 1, miniResult.toString());
           this.CalcAddSub();
         }
       }
     }
-    this.resultDis = this.cacheArr.join('');
-    this.display = this.resultDis;
+    this.finalResult = this.storeArray.join('');
+    this.display = this.finalResult;
   }
   //Wegen Rundung schauen 0,1 + 0,2
   mul(preOperand: string, afterOperand: string) {
@@ -161,9 +156,9 @@ export class AppComponent {
     this.saveNum = '';
     this.display = '';
     this.displaySmall = '';
-    this.cache2 = '';
-    this.cacheArr = [];
-    this.resultDis = '';
+    this.cache = '';
+    this.storeArray = [];
+    this.finalResult = '';
   }
 
   delete(): void {
